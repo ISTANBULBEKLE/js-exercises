0b995233358cc91f1cde0bf6f4862f297f376a8e
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-07-22 21:19:35 +0100

    exercise.js is revised

--- week-5/InClass/A-dom-manipulation/exercise.js
+++ week-5/InClass/A-dom-manipulation/exercise.js
@@ -15,7 +15,7 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
-/* let allPElements = document.querySelectorAll('p');
+let allPElements = document.querySelectorAll('p');
 console.log(allPElements);
 
 let firstDivElement = document.querySelector(".site-header.container");
@@ -28,7 +28,7 @@ console.log(jumbotronT);
 
 let primaryContentP= document.querySelectorAll(".primary-content p");
 
-console.log(primaryContentP); */
+console.log(primaryContentP);
 
 
 /*
@@ -39,13 +39,14 @@ When a user clicks the 'ALERT' button, an alert box should pop up with the text 
 
 
 */
-/* let myButton = document.querySelector("#alertBtn");
+
+let myButton = document.querySelector("#alertBtn");
 myButton.addEventListener("click", alertSomething);
 
 function alertSomething() {
   alert("Thanks for visiting Bikes for Refugees!");
 }
- */
+
 
 /*
 Task 3
@@ -53,13 +54,14 @@ Task 3
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
-/* let myBackgroundColor = document.getElementById("bgrChangeBtn");
+
+let myBackgroundColor = document.getElementById("bgrChangeBtn");
 myBackgroundColor.addEventListener("click", changeBackGroundColor);
 
 function changeBackGroundColor() {
     let selectBodyElement= document.querySelector("body");
     selectBodyElement.style.backgroundColor= "red";
-} */
+}
 
 /*
 Task 4
@@ -68,7 +70,7 @@ Task 4
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
 
-/* let addNewParagraph = document.querySelector("#addTextBtn");
+let addNewParagraph = document.querySelector("#addTextBtn");
 addNewParagraph.addEventListener("click", addSomeMoreText);
 
 function addSomeMoreText() {
@@ -76,7 +78,7 @@ function addSomeMoreText() {
     paragraph.textContent = "Read more below."; 
   let buttonSame = document.querySelector(".buttons")
   buttonSame.appendChild(paragraph);
-} */
+}
 
 /*
 Task 5
@@ -85,13 +87,19 @@ Task 5
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
 
-let linksPage = document.querySelector("#largerLinksBtn");
-linksPage.addEventListener("click", textSizeGetBigger);
-let myfontSize = 16;
+let largeButton = document.getElementById("largerLinksBtn");
+console.log (largeButton);
+largeButton.addEventListener("click", textSizeBigger);
+//let myfontSize = 26;
 
-function textSizeGetBigger() {
+function textSizeBigger() {
   let allAnchorEls = document.getElementsByTagName("a");
   //console.log("test")
-  myfontSize ++;
-  allAnchorEls.style.fontSize = `${myfontSize}px`;
+  //myfontSize ++;
+    console.log(allAnchorEls);
+  for (let i=0; i < allAnchorEls.length; i++){
+      allAnchorEls[i].style.fontSize = "50px";
+      //`${myfontSize}px`;
+  }
+  
 }
